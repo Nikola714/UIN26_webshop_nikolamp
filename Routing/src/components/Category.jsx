93a98d97f1ce19cb23cb9ebc953863f3980a2ba5
Nikola714 <nikolamp@hiof.no>
2026-02-16 13:59:43 +0100

    API
    forelesning om API

--- Routing/src/components/Category.jsx
+++ Routing/src/components/Category.jsx
@@ -1,15 +1,36 @@
 // Importerer useParams-hooken fra react-router-dom
 // useParams brukes for å hente URL-parametere (f.eks :slug)
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function Category(){
+
+    const [apiData, setApiData] = useState([])
 
     // useParams returnerer et objekt med alle parametere fra URL-en
     // Siden vi har definert path=':slug' i App.jsx, vil den hente verdien som står der
     // Eksempel: /categories/sko → slug = "sko"
     const { slug } = useParams()
+    const getSingleData = async()=>{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${slug}`)
+    const data = await response.json()
+    setApiData(data)
+    }
 
+    console.log(apiData)
+
+    useEffect(()=>{
+        getSingleData()
+    },[slug])
     // Returnerer JSX
     // Viser verdien av slug inne i en <h1>-tag
-    return <h1>{slug}</h1>
+    return (
+        <main>
+            <h1>{apiData?.name}</h1>
+            <section>
+                <h2>Bilder</h2>
+                <img src={apiData?.sprites?.front_default} alt={apiData?.name}/>
+            </section>
+        </main>
+    )
 }
